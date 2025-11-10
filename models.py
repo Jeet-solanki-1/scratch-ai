3b242d0cb111f74a40453f5cf6595440151e5755
Jeet-solanki-1 <solankijeet258@gmail.com>
2025-11-11 00:18:06 +0530

    wip(core): interface created for model.py and serialize.py

--- models.py
+++ models.py
@@ -1,1 +1,17 @@
  #TODO small dataclasses (Fact)
+ """
+ # models.py
+from dataclasses import dataclass
+
+@dataclass
+class Fact:
+    key: str
+    value: str
+"""
+from dataclasses import dataclass
+
+@dataclass
+class Fact:
+    key:str
+    value:str
+    
--- serializer.py
+++ serializer.py
@@ -1,1 +1,20 @@
  #TODO (optional) serialization interface
+ """
+ # serializer.py
+from typing import Dict, Any
+
+class Serializer:
+    def serialize(self, data: Dict[str, Any]) -> str:
+        Return a serialized string from data (e.g., JSON).
+        pass
+
+    def deserialize(self, payload: str) -> Dict[str, Any]:
+       Parse payload and return a dict
+        pass
+"""
+from typing import Dict, Any
+class Serializer:
+    def serialize(self,data:Dict[str,Any]) -> str:
+        pass
+    def deserialize(self,payload:str) -> Dict[str,Any]:
+        pass
